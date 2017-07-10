2080d3f91385dba01ac7c0917b2fe3710458120a
denniw <dennyclue@gmail.com>
2017-07-10 15:41:26 +0700

    SIMPEDA

---
+++ pelanggan/pages/pesansukses.php
@@ -0,0 +1,1 @@
+<h2>Selamat, Berita telah ditambahkan</h2>
---
+++ pelanggan/template/footer.php
@@ -0,0 +1,18 @@
+					</div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+	</div>
+
+
+    <script src="template/assets/js/jquery.min.js"></script>
+    <script src="template/assets/js/bootstrap.min.js"></script>
+    <script src="template/assets/js/metisMenu.min.js"></script>
+    <script src="template/assets/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
