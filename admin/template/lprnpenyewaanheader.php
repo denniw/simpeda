bb10fd00cf0ce039618033244c68af757163a06a
CallMeBonca <adityareiza@gmail.com>
2017-07-10 16:49:05 +0700

    laporanpenyewaan admin

---
+++ admin/template/lprnpenyewaanheader.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Admin SIMPEDA</title>
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/css/metisMenu.css" rel="stylesheet">
+	<link href="assets/css/sb-admin-2.css" rel="stylesheet">
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+
+<body>
+
+	<div id="wrapper">
+
+		<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index-admin.html">SIMPEDA</a>
+			</div>
+
+
+			<ul class="nav navbar-top-links navbar-right">
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+						<i class="fa fa-envelope fa-fw"></i> <i class="fa fa-caret-down"></i>
+					</a>
+					<ul class="dropdown-menu dropdown-messages">
+						<li>
+							<a href="#">
+								<div>
+									<strong>ID Pel : P1009822</strong>
+									<span class="pull-right text-muted">
+										<em>Yesterday</em>
+									</span>
+								</div>
+								<div>Bemper bagian depan pecah</div>
+							</a>
+						</li>
+						<li class="divider"></li>
+						<li>
+							<a href="#">
+								<div>
+									<strong>ID Pel : P1002322</strong>
+									<span class="pull-right text-muted">
+										<em>Yesterday</em>
+									</span>
+								</div>
+								<div>Menunggu Verifikasi Penyewaan</div>
+							</a>
+						</li>
+						<li class="divider"></li>
+						<li>
+							<a href="#">
+								<div>
+									<strong>System</strong>
+									<span class="pull-right text-muted">
+										<em>Today</em>
+									</span>
+								</div>
+								<div>Upload File Berhasil</div>
+							</a>
+						</li>
+						<li class="divider"></li>
+						<li>
+							<a class="text-center" href="#">
+								<strong>Read All Messages</strong>
+								<i class="fa fa-angle-right"></i>
+							</a>
+						</li>
+					</ul>
+
+				</li>
+
+				
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+						<i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+					</a>
+					<ul class="dropdown-menu dropdown-user">
+						<li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+						</li>
+						<li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+						</li>
+						<li class="divider"></li>
+						<li><a href="admin-login.html"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+						</li>
+					</ul>
+
+				</li>
+
+			</ul>
---
+++ admin/template/main-lprnpenyewaan.php
@@ -0,0 +1,8 @@
+<?php 
+		include_once 'lprnpenyewaanheader.php';
+		include_once 'lprnpenyewaansidebar.php';
+		include_once 'lprnpenyewaancontent.php';
+
+
+
+ ?>
